7555a9460b1d516990aab7e214d282d33e5b49a3
Alan McClain <mcclai54@msu.edu>
2019-02-05 18:00:27 -0500

    Update licorData.R
    refactored comments, removed unnecessary ETR/4 calculation

--- R/licorData.R
+++ R/licorData.R
@@ -15,8 +15,6 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
   #determine if its an xlsx...
   excel <- regexpr(".xlsx$",location)>=0
   if(excel){  #NOT PREFERED
-    #paste("This function only works on the no-extension tsv, not on xlsx files (R does not know how to read in formulas that include \"IF\" statements)")
-    #return()
     suppressMessages(data <- readxl::read_excel(path = location,sheet = 1,col_names = F))
     maxCols <- length(data)
     data <- tibble::as_tibble(data)
@@ -24,24 +22,16 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
     makeConstCol <- F
 
   } else {
-    maxCols <- max(count.fields(file=location,sep = "\t",quote = ""))
+    maxCols <- max(count.fields(file=location,sep = "\t",quote = "")) #This logic is needed to get around the long, narrow header at the top of the TSV
     data <- read.table(location,sep="\t",quote="",dec=".",stringsAsFactors = FALSE,blank.lines.skip = FALSE,skipNul = FALSE,comment.char="",row.names = NULL,col.names = 1:maxCols)
-    if(is.na(data[1,length(data)])){
+    if(is.na(data[1,length(data)])){  #Terminal \t at the end of every line
       data <- data[,-length(data)]
       maxCols <- maxCols -1
     }
     data <- tibble::as_tibble(data)
   }
-  #the maxCols variable is the number of variables, which we use when reading in the data
 
-  #this if statement deletes a blank column at the end
-  #by default, the licor data tsv has a \t at the end of every line, even though there is no data afterwards
-  #so this creates a blank column
-
-
-  #first find where the sysconst lines are for creating columns for constant changes
-
-  #store in the variables which change
+  #store in the variables which change and where
   if(makeConstCol){
     oxylocs <- dfPullConst(df=data,label = "SysConst:Oxygen")
     slocs <- dfPullConst(df=data,label = "Const:S")
@@ -49,8 +39,7 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
 
   #this while loop eliminates all the non-data at the top of the page.
   counter <- 0
-  #the purpose of the counter variable is to track how many lines of text we delete
-  #for adjustments to the sysconst columns later
+  #track how many lines of text we delete for adjustments to the sysconst columns later
   tester <- F
 
   while (!tester){
@@ -59,17 +48,16 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
       counter <- counter+1
     } else {
       tester <- T
-      data <- data[-1,]
+      data <- data[-1,] #delete the top row, which is not useful to us (category)
       counter <- counter+1
     }
   }
 
-  colnames(data)<-as.character(unlist(data[1,])) #turn the header line into column names
+  colnames(data)<-as.character(unlist(data[1,]))
   data<- data[-1,]
-  #delete the header line (it's saved as colnames) then delete the units line
-  data<- data[-1,]
+  data<- data[-1,] #delete header and unit lines
+  counter <- counter+2
   data <- tibble::set_tidy_names(data,quiet = T) #there are like 5 columns all called "time." this renames them to time..2,time..3 etc
-  counter <- counter+2 #increment counter for the header and unit lines
   #use counter var to adjust the "row" level on the slocs and oxylocs
   if(makeConstCol){
     slocs$row <- slocs$row - counter
@@ -90,16 +78,13 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
     coms <- c()
     comtimes<- c()
     while(counter < length(data$obs)+1){
-      if(data[counter,maxCols]==""){ #this is how we know it's a comment
-        #all comments are stored in the first 2cells
-        if(makeCommentsCol){
+      if(data[counter,maxCols]==""){ #this is how we know it's a comment (only has data in the first two columns)
+        if(makeCommentsCol){#we actually want to save the comment and put it in a new column
           comlocs <- rbind(comlocs,counter-1)
           comtimes <- rbind(comtimes,data[counter,1])
-          #we actually want to save the comment and put it in a new column
           coms <- rbind(coms,data[counter,2])
         }
         data <- data[-counter,] #remove the row with the comment
-        #deleting the line essentially advances the counter, so we don't want to advance it again
       }else
       {
         counter <- counter+1
@@ -109,7 +94,7 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
     #if there are no comments, you get errors with setting names and zero index for loops
     if(!is.null(comlocs)){
       commentdf <- data.frame("hhmmss" = comtimes, "Comments" = coms,stringsAsFactors = F)
-      commentdf <- magrittr::set_colnames(commentdf, c("hhmmss", "Comments")) #it tries to preserve the name for whatever reason...
+      commentdf <- magrittr::set_colnames(commentdf, c("hhmmss", "Comments"))
       colnames(data)[grep("hhmmss",colnames(data))[1]]<- "hhmmss" #rename first instance of hhmmss to just hhmmss for sorting
       data <- tibble::add_column(data,"Comments"=NA,.before=2)
       counter <- length(comlocs)
@@ -123,8 +108,8 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
 
   }
   if(makeCommentsCol & excel){
-    colnames(data)[grep("hhmmss",colnames(data))[1]]<- "hhmmss" #rename first instance of hhmmss to just hhmmss for sorting
-    data2 <- readxl::read_excel(path = location,sheet = 2,col_names = F) #on the xlsx comments are stored on page 2
+    colnames(data)[grep("hhmmss",colnames(data))[1]]<- "hhmmss" #rename first instance of hhmmss (there are multiple) to just hhmmss for sorting
+    data2 <- readxl::read_excel(path = location,sheet = 2,col_names = F) #in the xlsx comments are stored on page 2
     commentlocs <- grep(pattern = "[0:9]{2}",data2$..1) #only the comments have got numbers at the front of them on page 2
     #that seems questionable - not 100% sure this is the case
     comments <- data2$..2[commentlocs]
@@ -144,7 +129,6 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
     counter <- 1
     while(counter < length(important_data$CO2_r_sp)){
       if(is.na(important_data$CO2_r_sp[counter])){
-        #deleting the line essentially advances the counter, so we don't want to advance it again
         important_data <- important_data[-counter,]
       }else
       {
@@ -152,15 +136,13 @@ licorData <- function(location, returnImportant = F, purgeComments = T, makeCons
       }
     }
 
-    #if you don't do this you end up with a bunch of points where ETR is 0, not NA
-    #I have no idea why this is.  All I know is it works.
+    #if you don't do this you end up with a bunch of points where ETR is 0, not NA - graphs very poorly
     for(i in 1:length(important_data$ETR)){
       if(is.na(important_data$ETR[i])){
         important_data$ETR[i]<- ""
       }
     }
     important_data <-  dplyr::mutate_at(important_data,vars(1:10),as.numeric)
-    important_data$ETR <- important_data$ETR/4
     return(list(data,important_data))
   } else{
     return(data)
